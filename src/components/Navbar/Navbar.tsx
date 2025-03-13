52110a1114ae9794203d5e00c5fc413547d30e80
Mikolaj Lora <mikolaj.lora@gmail.com>
2025-03-13 18:39:04 +0100

    Adding some npm's. Start working with navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,24 @@
 
 import './Navbar.scss'
-const Navbar = () => {
+import { SelectedPage } from '../../shared/types.helper';
+import { useState } from 'react';
+import useMediaQuery from '../../hooks/useMediaQuery';
+
+
+type Props = {
+  isTopOfPage: boolean;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Navbar = ({isTopOfPage,selectedPage,setSelectedPage}:Props) => {
+
+
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1265px)");
+
+
+
   return (
     <nav>Navbar</nav>
   )
---
+++ src/shared/types.helper.ts
@@ -0,0 +1,12 @@
+
+export enum SelectedPage {
+    Home = "home",
+    OurMission= "ourmission",
+    WhyUs = "whyus",
+    OurValue = "ourvalue",
+    Services = "services",
+    Testimonials = "testimonials",
+    ContactUs = "contact",
+
+    
+  }
